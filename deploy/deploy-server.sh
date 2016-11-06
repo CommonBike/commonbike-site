337832ec2b8cce9e1c0ad8e7f7c7a6a4b24d1d36
Bart <bart@tuxion.nl>
2016-11-06 17:15:36 +0100

    update(docs) Updated server deploy script

--- deploy/deploy-server.sh
+++ deploy/deploy-server.sh
@@ -1,7 +1,6 @@
-cd ~/commonbike-site/bundle/programs/server
+cd /home/tuxion/commonbike-site/bundle/programs/server
 export PORT=3004
 export ROOT_URL=https://go.common.bike/
 export MONGO_URL=mongodb://localhost:27017/commonbike
 npm install
-cd ../../../
-forever start bundle/main.js
+forever start /home/tuxion/commonbike-site/bundle/main.js
