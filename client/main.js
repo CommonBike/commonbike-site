6b30b11c7eacade3d6e833f58efbeeb7acc15d6e
Eric van Riet Paap <ericvrp@gmail.com>
2016-12-19 20:38:16 +0100

    First working version with react-router. Next: more routes and authentication checks

--- client/main.js
+++ client/main.js
@@ -1,1 +1,29 @@
-Meteor.subscribe('locations');
+import React from 'react'
+import {render} from 'react-dom'
+import Router from 'react-router/BrowserRouter'
+import Match from 'react-router/Match'
+import Miss from 'react-router/Miss'
+
+import UserApp from '/imports/components/UserApp/UserApp.jsx'
+import Landing from '/imports/components/Landing/Landing.jsx'
+import CommonBikeUI from '/imports/commonbike-ui.jsx'
+import ContentPage from '/imports/components/ContentPage/ContentPage.jsx'
+import Join from '/imports/components/Join/Join.jsx'
+import NoMatch from '/imports/components/NoMatch/NoMatch.jsx'
+
+Meteor.subscribe('locations')
+
+const App = () => (
+  <Router>
+    <div>
+      <Match exactly pattern='/' component={Landing}/>
+      <Match pattern='/commonbike-ui' component={CommonBikeUI}/>
+      {/* <Match pattern='/join' component={<ContentPage><Join /></ContentPage>} /> */}
+      <Miss component={NoMatch}/>
+    </div>
+  </Router>
+)
+
+Meteor.startup(() => {
+  render(<App/>, document.getElementById('root'))
+})
