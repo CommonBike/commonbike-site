c5d54ada1113deddabf268d552c5b5580fbe2459
Eric van Riet Paap <ericvrp@gmail.com>
2016-12-23 10:00:30 +0100

    Rebased on develop after feature/testdata had been merged there.

--- imports/components/NoMatch/NoMatch.jsx
+++ imports/components/NoMatch/NoMatch.jsx
@@ -5,16 +5,6 @@ const NoMatch = ({ location }) => (
     <h2>Whoops</h2>
     <p>Sorry but {location.pathname} didn’t match any pages</p>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
 )
 
 export default NoMatch
-=======
-)
->>>>>>> define some basic routes
-=======
-)
-
-export default NoMatch
->>>>>>> First working version with react-router. Next: more routes and authentication checks
