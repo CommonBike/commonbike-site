8bfb6b5a8921e5fd5b57ec4cd63e85daa2686852
Peter <pveijk@gmail.com>
2017-05-13 20:43:15 +0000

    deploy scripts

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,4 +2,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo heroku process
 heroku container:push web --app $HEROKU_APP_NAME
