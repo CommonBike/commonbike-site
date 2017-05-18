f1175e6674fc37f4ed2f176764d508da8ea1599c
Eric van Riet Paap <ericvrp@gmail.com>
2017-05-18 17:39:36 +0200

    Export CommonBikeCoin functions (no longer as default, interface will change in the future) and add the token abi

--- app/server/api/CBC.js
+++ app/server/api/CBC.js
@@ -1,25 +1,33 @@
 var bip39 = require("bip39")
-// // var Web3 = require("web3")
 var HDWalletProvider = require("truffle-hdwallet-provider") // https://github.com/trufflesuite/truffle-hdwallet-provider
+var Web3 = require("web3")
+
+var token_abi = require('./CBCabi.json')
 
 import { Settings } from '/imports/api/settings.js';
+var settings = Settings.findOne({});
+const token_address = settings.bikecoin.token_address || '0x560dae10c6f6655588d0d3596e757e3825dd55cfd331056ef69e4dc67a308cd5'
+const provider_url  = settings.bikecoin.provider_url  || 'https://ropsten.infura.io/sCQUO1V3FOoOUWGZBtig'
 
-const newCBCkeypair = () => { // CBC = CommonBikeCoin
-  const seedPhrase  = bip39.generateMnemonic()
 
-  const providerURL = 'https://ropsten.infura.io/sCQUO1V3FOoOUWGZBtig'
-  const provider    = new HDWalletProvider(seedPhrase, providerURL)
-  // const web3        = new Web3(this.provider)
+export const newCBCkeypair = () => { // CBC = CommonBikeCoin
+  const seedPhrase = bip39.generateMnemonic()
+  const provider   = new HDWalletProvider(seedPhrase, provider_url)
 
-  const keypair = {
+  return {
     address: '0x' + provider.wallet.getAddress().toString("hex"),
-    privateKey: seedPhrase,
+    privatekey: seedPhrase,
   }
-  // console.warn(keypair)
-  return keypair
 }
-
-export default newCBCkeypair
-
 global.newCBCkeypair = newCBCkeypair
-// global.Web3 = Web3
+
+
+export const getToken = (seedPhrase) => {
+  const provider = new HDWalletProvider(seedPhrase, provider_url)
+  const web3     = new Web3(provider)
+  return web3.eth.contract(token_abi).at(token_address)
+}
+global.getToken = getToken
+
+
+// export default newCBCkeypair
