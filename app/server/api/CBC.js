8535d006dca0d44c3cef5a95e8b3aa02052730f3
Eric van Riet Paap <ericvrp@gmail.com>
2017-05-18 17:17:41 +0200

    User "meteor npm install" because "npm install" gives an error when meteor imports truffle-hdwallet-provider

--- app/server/api/CBC.js
+++ app/server/api/CBC.js
@@ -1,29 +1,25 @@
-var HDWalletProvider = require("truffle-hdwallet-provider") // https://github.com/trufflesuite/truffle-hdwallet-provider
-var Web3 = require("web3")
 var bip39 = require("bip39")
+// // var Web3 = require("web3")
+var HDWalletProvider = require("truffle-hdwallet-provider") // https://github.com/trufflesuite/truffle-hdwallet-provider
 
 import { Settings } from '/imports/api/settings.js';
 
 const newCBCkeypair = () => { // CBC = CommonBikeCoin
-  console.warn('newCBCkeypair start')
-
-  // const providerURL = Settings.findOne(
-
-
   const seedPhrase  = bip39.generateMnemonic()
+
   const providerURL = 'https://ropsten.infura.io/sCQUO1V3FOoOUWGZBtig'
   const provider    = new HDWalletProvider(seedPhrase, providerURL)
-  const web3        = new Web3(this.provider)
+  // const web3        = new Web3(this.provider)
 
   const keypair = {
     address: '0x' + provider.wallet.getAddress().toString("hex"),
     privateKey: seedPhrase,
   }
-  console.warn('newCBCkeypair end', keypair)
-
+  // console.warn(keypair)
   return keypair
 }
 
 export default newCBCkeypair
 
 global.newCBCkeypair = newCBCkeypair
+// global.Web3 = Web3
